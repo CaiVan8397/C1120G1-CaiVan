162587f6132b8d11edb3f8941759f16a6d6443b7
Caivan-c1120g1 <caivan.8397@gmail.com>
2020-12-08 08:22:00 +0700

    upload bai tap

--- module 1/Bài 6/sum.js
+++
@@ -1,6 +0,0 @@
-function sum() {
-    let a=parseInt(prompt('nhap so thu nhat'));
-    let b=parseInt(prompt('nhap so thu 2'));
-    sum=a+b;
-
-}
---
+++ module 1/bài 7/Thực hành/dientich.js
@@ -0,0 +1,6 @@
+function dientich() {
+    let width = parseInt(document.getElementById("chieudai");
+    let height = parseInt(document.getElementById("chieurong");
+    let dientich=width*height;
+    document.getElementById(dt).innerText = "Dientich la" + dientich
+}
