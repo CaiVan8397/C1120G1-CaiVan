9ae22cc082d6a0abb21d06ea50d5cc5a8f4c3f03
Caivan-c1120g1 <caivan.8397@gmail.com>
2020-12-08 08:28:38 +0700

    bai tap

--- module 1/bài 7/Thực hành/dientich.js
+++ module 1/bài 7/Thực hành/dientich.js
@@ -1,6 +1,8 @@
 function dientich() {
-    let width = parseInt(document.getElementById("chieudai");
-    let height = parseInt(document.getElementById("chieurong");
-    let dientich=width*height;
-    document.getElementById(dt).innerText = "Dientich la" + dientich
+    let width =document.getElementById("chieudai");
+    let height =document.getElementById("chieurong");
+    let a=parseInt(width.value);
+    let b=parseInt(height.value);
+    let S=a*b;
+    document.getElementById("dt").innerText = "Dientich la " +S;
 }
