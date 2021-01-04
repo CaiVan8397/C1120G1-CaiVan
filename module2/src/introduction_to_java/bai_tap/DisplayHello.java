69941ac4cb61d712d0bab51f0048a41b03b6e8f4
Caivan-c1120g1 <caivan.8397@gmail.com>
2021-01-04 08:31:27 +0700

    module 2 bai 2

---
+++ module2/src/introduction_to_java/bai_tap/DisplayHello.java
@@ -0,0 +1,12 @@
+package introduction_to_java;
+
+import java.util.Scanner;
+
+public class DisplayHello {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Hello " + name);
+    }
+}
