77335562e40d43358bfffb9a658627d5571ce645
Caivan-c1120g1 <caivan.8397@gmail.com>
2021-01-07 16:11:38 +0700

    bai tap

---
+++ module2/src/access_modifier/thuc_hanh/a/A.java
@@ -0,0 +1,34 @@
+package access_modifier.thuc_hanh.a;
+
+import access_modifier.thuc_hanh.b.B;
+//default
+//public class A {
+//     package pack;
+//
+//    class A{
+//
+//        void msg(){System.out.println("Hello");}
+//
+//    }
+//}
+
+// Protected
+//class A extends B {
+//
+//    public static void main(String args[]){
+//
+//        B obj = new B();
+//
+//        obj.msg();
+//
+//    }
+//
+//}
+
+public class  A{
+
+    public void msg(){System.out.println("Hello");}
+
+}
+
+
